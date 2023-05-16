bd3bc05a7da5a23fbe371ba0f2022b461aef6895
uthman1 <uthman@e-shore.com.my>
2023-05-16 20:22:53 +0800

    First Commit

---
+++ Project1_Alarm_Intruder.ino
@@ -0,0 +1,78 @@
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+SoftwareSerial bluetoothSerial(11,10);
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+int pirPinNumber = 2;              
+int pirState;
+int isMotion = false;
+
+int ledPinNumber = 3;
+int buzzerPinNumber = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pirPinNumber,INPUT);
+  pinMode(ledPinNumber,OUTPUT);
+  Serial.begin(9600);
+  bluetoothSerial.begin(9600);
+  lcd.init();
+  
+  Serial.println("Silent Alarm ");
+  Serial.println("Initializing System");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  pirState = digitalRead(pirPinNumber);
+  if(pirState == HIGH){
+    lcd.backlight();
+    lcd.print("Motion Detected");
+    delay(500);
+    lcd.clear();
+    // Will sent to phone using bluetooth
+    sendBluetoothSetAlarmSignal();
+
+    lcd.print("Alarm Signal is Sent");
+    delay(1000);
+    lcd.clear();
+    lcd.noBacklight();
+
+    if (isMotion == false) { 
+      isMotion = true;       
+    }
+  
+  }
+  else{
+    if(isMotion == true){   
+      lcd.backlight();
+      lcd.print("Clearing Alarm Signal");
+      delay(500);
+      // Will sent to phone using bluetooth
+      sendBluetoothClearAlarmSignal();
+
+      lcd.clear();
+      lcd.print("Alarm Clear");
+      delay(500);
+      lcd.clear();
+      lcd.noBacklight();
+      
+      isMotion = false; 
+    }
+  }
+  
+
+  digitalWrite(ledPinNumber,isMotion);
+  digitalWrite(buzzerPinNumber,isMotion);
+}
+
+void sendBluetoothSetAlarmSignal(){
+  bluetoothSerial.println("Intruder Alert");
+}
+
+void sendBluetoothClearAlarmSignal(){
+  bluetoothSerial.println("No Intruder");
+}
